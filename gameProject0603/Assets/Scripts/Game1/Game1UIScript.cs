a5df0e7f59192582ae5320a15e898bbc627bb379
mai yurman <mai.yurman777@gmail.com>
2022-03-08 14:33:35 +0200

    game1

---
+++ gameProject0603/Assets/Scripts/Game1/Game1UIScript.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game1UIScript : MonoBehaviour
+{
+    public navigation messageBtn;
+    public GameObject openScreen;
+    public GameObject Screen1;
+
+    public void btnMessageBtn()
+    {
+        Debug.Log("הפעלת כפתור הודעה");
+        messageBtn.enableBtn();
+
+    }
+
+    public void openScreen1()
+    {
+        openScreen.SetActive(false);
+        Screen1.SetActive(true);
+    }
+
+}
