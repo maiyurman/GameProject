762ad1e6d23296d88a2ec4391eab1a67ac3b3667
mai yurman <mai.yurman777@gmail.com>
2022-04-21 00:33:10 +0300

    צאט ומשחק 1
    לוגיקה גמורה אבל נשאר לעשות בעיקר את ההסברים של המשפיען והטקסטים

--- gameProject0603/Assets/Scripts/Game1/Game1Logic.cs
+++ gameProject0603/Assets/Scripts/Game1/Game1Logic.cs
@@ -4,52 +4,268 @@ using UnityEngine;
 
 public class Game1Logic : MonoBehaviour
 {
-
+    //משתנים משחק 1
     private Game1UIScript Game1UIScript;
-    private string writeOnBtn;
+    public Chat chatGame1;
+    private int tryGame1;
+    public GameObject finishGame1;
 
+    //משתנים משחק 2
+    public Chat chatGame2;
+    private int tryGame2;
+    public GameObject finishGame2;
+
+    //משתנים משחק 3
+    public Chat chatGame3;
+    private int tryGame3;
+    public GameObject finishGame3;
+
+    //סטורי
+    private loadStoryBtn loadStoryBtn;
+    bool isAnswerTrue = false;
 
     void Start()
     {
+        loadStoryBtn = GameObject.Find("levels").GetComponent<loadStoryBtn>();
         Game1UIScript = transform.gameObject.GetComponent<Game1UIScript>();
+
+        //משחק 1
+        chatGame1.setChatTitle("תמר לוי");
+        chatGame1.addVideo(Chat.Direction.RECEIVE, "6");
+        tryGame1 = 2;
+
+        //משחק 2
+        chatGame2.setChatTitle("אור בן ישי");
+        chatGame2.addVideo(Chat.Direction.RECEIVE, "6");
+        tryGame2 = 2;
+
+        //משחק 3
+        chatGame3.setChatTitle("'אמה דוידוביץ");
+        chatGame3.addVideo(Chat.Direction.RECEIVE, "6");
+        tryGame3 = 2;
     }
-    bool isAnswerTrue = false;
-    int numOfTrying = 0;
 
 
-    public void pressTrueAnswer()
+    //public void B1()
+    //{
+    //    chat.addVideo(Chat.Direction.RECEIVE, "6");
+    //    chat.addPhoto(Chat.Direction.RECEIVE, "6");
+    //    chat.addTextMessage(Chat.Direction.RECEIVE, "sdfsdfs\ndsfsfsfs");
+    //    chat.addTextMessage(Chat.Direction.RECEIVE, "טקסט בלה בלה בלה גכדגלכחד דןגכדוג כדוגככ בלה בלה בלה 'יכוגדטכוןדג דגוכטוד9", 3);
+    //}
+    //public void B2()
+    //{
+    //    chat.addVideo(Chat.Direction.SEND, "2");
+    //    chat.addPhoto(Chat.Direction.SEND, "2");
+    //    chat.addTextMessage(Chat.Direction.SEND, "טקסט בלה בלה בלה גכדגלכחד דןגכדוג כדוגככ בלה בלה בלה 'יכוגדטכוןדג דגוכטוד9", 3);
+    //}
+
+
+
+
+    //------------------------------------------------משחק 1 פידבקים
+
+    public void Game1TrueAnswer()
     {
         isAnswerTrue = true;
-        writeOnBtn = "את לא מצליחה לנהל את הזמן שלך נכון";
-        Game1UIScript.AnswerFeedback(numOfTrying, isAnswerTrue, writeOnBtn);
+        chatGame1.addTextMessage(Chat.Direction.SEND,"את לא מצליחה לנהל את הזמן שלך נכון", 3);
+        Game1feedbackforanswers();
     }
 
-    public void pressFalseAnswer1()
+    public void Game1FalseAnswer1()
     {
-        writeOnBtn = "החברים שלך לא מרוצים מההתנהגות שלך";
-        Game1UIScript.AnswerFeedback(numOfTrying, isAnswerTrue, writeOnBtn);
-        tryNumUp();
+        chatGame1.addTextMessage(Chat.Direction.SEND, "החברים שלך לא מרוצים מההתנהגות שלך", 3);
+        Game1feedbackforanswers();
+        tryGame1Down();
     }
 
-    public void pressFalseAnswer2()
+    public void Game1FalseAnswer2()
     {
-        writeOnBtn = "הבוס שלך לא מתחשב בך";
-        Game1UIScript.AnswerFeedback(numOfTrying, isAnswerTrue, writeOnBtn);
-        tryNumUp();
+        chatGame1.addTextMessage(Chat.Direction.SEND, "הבוס שלך לא מתחשב בך", 3);
+        Game1feedbackforanswers();
+        tryGame1Down();
     }
 
-    public void pressFalseAnswer3()
+    public void Game1FalseAnswer3()
     {
-        writeOnBtn = "את לומדת בזמן העבודה";
-        Game1UIScript.AnswerFeedback(numOfTrying, isAnswerTrue, writeOnBtn);
-        tryNumUp();
+        chatGame1.addTextMessage(Chat.Direction.SEND, "את לומדת בזמן העבודה", 3);
+        Game1feedbackforanswers();
+        tryGame1Down();
     }
 
-    public void tryNumUp()
+    public void Game1feedbackforanswers()
     {
-        if (numOfTrying != 2)
+        if (isAnswerTrue)
         {
-            numOfTrying++;
+            chatGame1.addTextMessage(Chat.Direction.RECEIVE, "ניהול זמן בהחלט יכול לעזור לי להתנהל טוב יותר", 2);
+            finishGame1.SetActive(true);
+            isAnswerTrue = false;
         }
+        else
+        {
+            if (tryGame1 == 2)
+            {
+                chatGame1.addTextMessage(Chat.Direction.RECEIVE, ",אני לא בטוחה שזה מקור הבעיה, נסה לבחור באפשרות אחרת", 2);
+            }
+            else if (tryGame1 == 1)
+            {
+                chatGame1.addTextMessage(Chat.Direction.RECEIVE, "צפייה חוזרת בסרטון למעלה תעזור לזהות את מקור הבעיה", 2);
+            }
+            else
+            {
+                chatGame1.addTextMessage(Chat.Direction.RECEIVE, ".אחרי שחשבתי על זה, הגעתי למסקנה שהבעיה שלי היא בכלל ניהול זמן לא נכון !צפיתי בסרטון שוב ומצאתי את המכנה המשותף אבל תודה על הרצון לעזור", 5);
+                finishGame1.SetActive(true);
+                isAnswerTrue = false;
+            }
+        }
+    }
+
+    //------------------------------------------------משחק 2 פידבקים
+
+    public void Game2TrueAnswer()
+    {
+        isAnswerTrue = true;
+        chatGame2.addTextMessage(Chat.Direction.SEND, "אתה מתנהג בפזיזות ולא שוקל מילים", 3);
+        Game2feedbackforanswers();
+    }
+
+    public void Game2FalseAnswer1()
+    {
+        chatGame2.addTextMessage(Chat.Direction.SEND, "קבעת שיעור נהיגה ארוך מידי", 3);
+        Game2feedbackforanswers();
+        tryGame2Down();
+    }
+
+    public void Game2FalseAnswer2()
+    {
+        chatGame2.addTextMessage(Chat.Direction.SEND, "ההורים שלך מתנהגים בחוסר סבלנות", 3);
+        Game2feedbackforanswers();
+        tryGame2Down();
+    }
+
+    public void Game2FalseAnswer3()
+    {
+        chatGame2.addTextMessage(Chat.Direction.SEND, "המורה לנהיגה לא סומך עליך", 3);
+        Game2feedbackforanswers();
+        tryGame2Down();
+    }
+
+    public void Game2feedbackforanswers()
+    {
+        if (isAnswerTrue)
+        {
+            chatGame2.addTextMessage(Chat.Direction.RECEIVE, "!נכון, לחשוב לפני שאני פועל יכול לעזור לי, תודה על העזרה", 2);
+            finishGame2.SetActive(true);
+            isAnswerTrue = false;
+        }
+        else
+        {
+            if (tryGame2 == 2)
+            {
+                chatGame2.addTextMessage(Chat.Direction.RECEIVE, ".אני לא בטוח שזה מקור הבעיה, נסה לבחור באפשרות אחרת", 2);
+            }
+            else if (tryGame2 == 1)
+            {
+                chatGame2.addTextMessage(Chat.Direction.RECEIVE, "צפייה חוזרת בסרטון למעלה תעזור לזהות את מקור הבעיה", 2);
+            }
+            else
+            {
+                chatGame2.addTextMessage(Chat.Direction.RECEIVE, ".אחרי שחשבתי על זה, הגעתי למסקנה שהבעיה שלי היא שאני פזיז ולא חושב לפני שאני פועל !צפיתי בסרטון שוב ומצאתי את המכנה המשותף אבל תודה על הרצון לעזור", 5);
+                finishGame2.SetActive(true);
+                isAnswerTrue = false;
+            }
+        }
+    }
+
+    //------------------------------------------------משחק 3 פידבקים
+
+    public void Game3TrueAnswer()
+    {
+        isAnswerTrue = true;
+        chatGame3.addTextMessage(Chat.Direction.SEND, "את נהגת ביהירות בנוגע לקבלה ליחידה", 3);
+        Game3feedbackforanswers();
+    }
+
+    public void Game3FalseAnswer1()
+    {
+        chatGame3.addTextMessage(Chat.Direction.SEND, "את לא מוכנה להתפשר על התפקיד", 3);
+        Game3feedbackforanswers();
+        tryGame3Down();
+    }
+
+    public void Game3FalseAnswer2()
+    {
+        chatGame3.addTextMessage(Chat.Direction.SEND, "המבחנים היו קשים מידי", 3);
+        Game3feedbackforanswers();
+        tryGame3Down();
+    }
+
+    public void Game3FalseAnswer3()
+    {
+        chatGame3.addTextMessage(Chat.Direction.SEND, "שכחת להתכונן למבחנים", 3);
+        Game3feedbackforanswers();
+        tryGame3Down();
+    }
+
+    public void Game3feedbackforanswers()
+    {
+        if (isAnswerTrue)
+        {
+            chatGame3.addTextMessage(Chat.Direction.RECEIVE, "נכון, יש לי נטייה כזו אני צריכה לעבוד על זה! תודה על העזרה", 2);
+            finishGame3.SetActive(true);
+            isAnswerTrue = false;
+        }
+        else
+        {
+            if (tryGame3 == 2)
+            {
+                chatGame3.addTextMessage(Chat.Direction.RECEIVE, "אני לא בטוחה שזה מקור הבעיה, כדאי לבחור באפשרות אחרת", 2);
+            }
+            else if (tryGame3 == 1)
+            {
+                chatGame3.addTextMessage(Chat.Direction.RECEIVE, "צפייה חוזרת בסרטון למעלה תעזור לזהות את מקור הבעיה", 2);
+            }
+            else
+            {
+                chatGame3.addTextMessage(Chat.Direction.RECEIVE, ".אחרי שחשבתי על זה, הגעתי למסקנה שהבעיה שלי היא שהייתי יהירה מידי בנוגע לקבלה ליחידה. צפיתי בסרטון שוב ומצאתי את המכנה המשותף אבל תודה על הרצון לעזור", 5);
+                finishGame3.SetActive(true);
+                isAnswerTrue = false;
+            }
+        }
+    }
+
+    //הורדת מספר ניסיונות כלל המשחקים
+    public void tryGame1Down()
+    {
+        if (tryGame1 != 0)
+        {
+            tryGame1--;
+        }
+    }
+
+    public void tryGame2Down()
+    {
+        if (tryGame2 != 0)
+        {
+            tryGame2--;
+        }
+    }
+
+    public void tryGame3Down()
+    {
+        if (tryGame3 != 0)
+        {
+            tryGame3--;
+        }
+    }
+
+    public void finishGame()
+    {
+        //הפעלת כפתור סטורי
+        PlayerPrefs.SetInt("openGameMax", 1);
+        int myMaxLevel = PlayerPrefs.GetInt("openGameMax");
+        loadStoryBtn.enableStoryBtn(myMaxLevel);
+
+        //להוסיף החשכה של המסך עם הודעה שאפשר להיכנס לסטורי עם המשפיען
     }
 }
--- gameProject0603/Assets/Scripts/Game1/Game1UIScript.cs
+++ gameProject0603/Assets/Scripts/Game1/Game1UIScript.cs
@@ -5,107 +5,68 @@ using TMPro;
 
 public class Game1UIScript : MonoBehaviour
 {
+    private Game1Logic Game1LogicScript;
     public navigation messageBtn;
     public GameObject openScreen;
-    public GameObject Screen1;
-    public GameObject Screen2;
+    public GameObject game1ChatBox;
+    public GameObject game1Chat;
+    public GameObject game2ChatBox;
+    public GameObject game2Chat;
+    public GameObject game3ChatBox;
+    public GameObject game3Chat;
 
-    //----->נראות של התשובות
-    public GameObject try1Answer;
-    public GameObject try2Answer;
-    public GameObject try3Answer;
-
-    //-----> טקסטים של התשובות
-    public TextMeshProUGUI chatAnswer1;
-    public TextMeshProUGUI chatAnswer2;
-    public TextMeshProUGUI chatAnswer3;
-
-    public TextMeshProUGUI userchoose1;
-    public TextMeshProUGUI userchoose2;
-    public TextMeshProUGUI userchoose3;
-
-    //----> נראות הגלגלת
-    public GameObject scroolBar;
-
-    ///---> נראות חלון הצ'אטים 
-    public GameObject questionView;
-    public GameObject finishChat;
-
-    void Start()
+    private void Start()
     {
-        try1Answer.SetActive(false);
-        try2Answer.SetActive(false);
-        try3Answer.SetActive(false);
-        scroolBar.SetActive(false);
-        finishChat.SetActive(false);
-        questionView.SetActive(true);
+        Game1LogicScript = transform.gameObject.GetComponent<Game1Logic>();
+        messageBtn.disableBtn();
+        openScreen.SetActive(true);
+        game1ChatBox.SetActive(false);
+        game1Chat.SetActive(false);
+        game2ChatBox.SetActive(false);
+        game2Chat.SetActive(false);
+        game3ChatBox.SetActive(false);
+        game3Chat.SetActive(false);
     }
 
-    public void btnMessageBtn()
+    public void openMessageBtn()
     {
         messageBtn.enableBtn();
     }
 
-    public void openScreen1()
+    public void opengame1ChatBox()
     {
         openScreen.SetActive(false);
-        Screen1.SetActive(true);
+        game1ChatBox.SetActive(true);
     }
 
-    public void openScreen2()
+    public void opengame1Chat()
     {
-        Screen1.SetActive(false);
-        Screen2.SetActive(true);
+        game1ChatBox.SetActive(false);
+        game1Chat.SetActive(true);
     }
 
-    public void AnswerFeedback(int numOfTrying, bool isAnswerTrue, string writeOnBtn)
+    public void opengame2ChatBox()
     {
-            if (numOfTrying == 0)
-            {
-            try1Answer.SetActive(true);
-            userchoose1.text = writeOnBtn;
-                if (isAnswerTrue == true)
-                {
-                chatAnswer1.text = "כל הכבוד תשובה נכונה ועוד בפעם הראשונה!";
-                finishChat.SetActive(true);
-                questionView.SetActive(false);
-                }
-                else
-                {
-                chatAnswer1.text = "אני לא בטוחה שזה מקור הבעיה, נסה לבחור באפשרות אחרת";
-                }
-            }
-            else if (numOfTrying == 1)
-            {
-            scroolBar.SetActive(true);
-            try2Answer.SetActive(true);
-            userchoose2.text = writeOnBtn;
-                if (isAnswerTrue == true)
-                {
-                    chatAnswer2.text = "כל הכבוד תשובה נכונה ועוד בפעם השניה!";
-                    finishChat.SetActive(true);
-                    questionView.SetActive(false);
-                }
-                else
-                {
-                    chatAnswer2.text = "צפייה חוזרת בסרטון למעלה תעזור לזהות את מקור הבעיה";
-                }
-            }
-            else
-            {
-            try3Answer.SetActive(true);
-            userchoose3.text = writeOnBtn;
-            questionView.SetActive(false);
-            finishChat.SetActive(true);
-                if (isAnswerTrue == true)
-                {
-                    chatAnswer3.text = "כל הכבוד תשובה נכונה ועוד בפעם השלישית!";
-                }
-                else
-                {
-                    chatAnswer3.text = ".אחרי שחשבתי על זה, הגעתי למסקנה שהבעיה שלי היא בכלל ניהול זמן לא נכון! צפיתי בסרטון שוב ומצאתי את המכנה המשותף אבל תודה על הרצון לעזור";
-                }
-            }
-        }
+        game1Chat.SetActive(false);
+        game2ChatBox.SetActive(true);
+    }
+
+    public void opengame2Chat()
+    {
+        game2ChatBox.SetActive(false);
+        game2Chat.SetActive(true);
+    }
+
+    public void opengame3ChatBox()
+    {
+        game2Chat.SetActive(false);
+        game3ChatBox.SetActive(true);
+    }
+
+    public void opengame3Chat()
+    {
+        game3ChatBox.SetActive(false);
+        game3Chat.SetActive(true);
+    }
+
 }
-
