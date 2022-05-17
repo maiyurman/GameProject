25ce2746111170fb395bbe5b5178269fbe5c9855
mai yurman <mai.yurman777@gmail.com>
2022-05-18 00:04:50 +0300

    תיקונים ובדיקות

---
+++ gameProject0405/Assets/Scripts/Game1/Game1UIScript.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Game1UIScript : MonoBehaviour
+{
+    private Game1Logic Game1LogicScript;
+    public navigation messageBtn;
+    public GameObject openScreen;
+    public GameObject game1ChatBox;
+    public GameObject game1Chat;
+    public GameObject game2ChatBox;
+    public GameObject game2Chat;
+    public GameObject game3ChatBox;
+    public GameObject game3Chat;
+
+    public navigation nextBtn;
+
+    private void Start()
+    {
+
+        Game1LogicScript = transform.gameObject.GetComponent<Game1Logic>();
+        messageBtn.disableBtn();
+        openScreen.SetActive(true);
+        nextBtn.enableBtn();
+        game1ChatBox.SetActive(false);
+        game1Chat.SetActive(false);
+        game2ChatBox.SetActive(false);
+        game2Chat.SetActive(false);
+        game3ChatBox.SetActive(false);
+        game3Chat.SetActive(false);
+    }
+
+    public void openMessageBtn()
+    {
+        messageBtn.enableBtn();
+    }
+
+    public void opengame1ChatBox()
+    {
+        openScreen.SetActive(false);
+        game1ChatBox.SetActive(true);
+    }
+
+    public void opengame1Chat()
+    {
+        game1ChatBox.SetActive(false);
+        game1Chat.SetActive(true);
+    }
+
+    public void opengame2ChatBox()
+    {
+        game1Chat.SetActive(false);
+        game2ChatBox.SetActive(true);
+    }
+
+    public void opengame2Chat()
+    {
+        game2ChatBox.SetActive(false);
+        game2Chat.SetActive(true);
+    }
+
+    public void opengame3ChatBox()
+    {
+        game2Chat.SetActive(false);
+        game3ChatBox.SetActive(true);
+    }
+
+    public void opengame3Chat()
+    {
+        game3ChatBox.SetActive(false);
+        game3Chat.SetActive(true);
+    }
+
+    public void test()
+    {
+        Debug.Log("test");
+    }
+
+}
---
+++ gameProject0405/Assets/Scripts/Game5/Game5UIManager.cs
@@ -0,0 +1,175 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Game5UIManager : MonoBehaviour
+{
+    public GameObject round1;
+    public GameObject round2;
+    public GameObject round3;
+    public GameObject influenceFeedBack;
+    public navigation checkBtn;
+    private Game5DragLogic Game5DragLogic;
+    private Dragable currentdragable;
+    public GameObject feedbackwindow;
+    public GameObject feedbackAnotherTry;
+    public TextMeshProUGUI feedbackTxt;
+    public TextMeshProUGUI feedbackBtnTxt;
+    public int tryNum;
+    public int numRound;
+    private string message;
+
+    void Start()
+    {
+        initround();
+        tryNum = 0;
+        checkBtn.gameObject.transform.gameObject.SetActive(true);
+        Game5DragLogic = transform.gameObject.GetComponent<Game5DragLogic>();
+        feedbackwindow.SetActive(false);
+        numRound = 1;
+    }
+
+    public void initround()
+    {
+        influenceFeedBack.SetActive(false);
+        feedbackAnotherTry.SetActive(false);
+        round1.SetActive(false);
+        round2.SetActive(false);
+        round3.SetActive(false);
+    }
+
+    public void movetoPlace(Dragable dragable, DropArea dropArea)
+    {
+        currentdragable = dragable;
+        //הגדרה של הכפתור
+        Vector3 dragableplace = dragable.transform.position;
+        dragableplace = dropArea.transform.position;
+        //הדלקה של כפתור הבדיקה
+        checkBtn.enableBtn();
+        Debug.Log("btnOn");
+
+    }
+
+    public void checkBtnClick()
+    {
+        if (Game5DragLogic.isTrueAnswer == true)
+        {
+            feedbackTrue();
+        }
+        else
+        {
+            feedbackfalse();
+
+        }
+    }
+
+    public void feedbackTrue()
+    {
+        //מעלימים את כפתור הבדיקה
+        checkBtn.gameObject.transform.gameObject.SetActive(false);
+        //אנימציית נסיעה
+        message = "תשובה נכונה כל הכבוד!";
+        //פונקציה שמקפיצה חלון
+        feedbackwindowopen(message);
+    }
+
+    public void feedbackfalse()
+    {
+        if (tryNum == 0)
+        {
+            feedbackfalseTry();
+        }
+        else
+        {
+            feedbackfalseFinish();
+            feedbackFalseMessageRound();
+            feedbackwindowopen(message);
+        }
+
+    }
+
+    public void feedbackfalseTry()
+    {
+        currentdragable.returnToInitPosition();
+        feedbackAnotherTry.SetActive(true);
+        //הכפתור הופך ללא פעיל שוב
+        checkBtn.disableBtn();
+        tryNum++;
+    }
+
+    public void feedbackfalseFinish()
+    {
+        Debug.Log("תשובה לא נכונה - מתן הפידבק להמשך משחק");
+        //מעלימים את כפתור הבדיקה
+        checkBtn.gameObject.transform.gameObject.SetActive(false);
+
+    }
+
+    public void feedbackwindowopen(string message)
+    {
+        feedbackwindow.SetActive(true);
+        feedbackTxt.text = message;
+
+        if (numRound == 1 | numRound == 2)
+        {
+            feedbackBtnTxt.text = "למשחק הבא";
+        }
+        else
+        {
+            feedbackBtnTxt.text = "לסיום המשחק";
+        }
+    }
+
+    public void feedbackFalseMessageRound()
+    {
+        if (numRound == 1)
+        {
+            message = "תשובתכם אינה נכונה המסלול המהיר ביותר הוא של הילה -רק במסלול זה השליח עובר פעם אחת בכל בית על מנת לחלק את המתנות";
+        }
+        else if (numRound == 2)
+        {
+            message = "תשובתכם אינה נכונה המסלול המהיר ביותר הוא של רותם -רק במסלול זה השליח עובר פעם אחת בכל בית על מנת לחלק את המתנות";
+        }
+        else
+        {
+            message = "תשובתכם אינה נכונה המסלול המהיר ביותר הוא של בן -רק במסלול זה השליח עובר פעם אחת בכל בית על מנת לחלק את המתנות";
+        }
+    }
+
+    public void finishRoundClick()
+    {
+        if (numRound == 1)
+        {
+            numRound++;
+            round1.SetActive(false);
+            round2.SetActive(true);
+            round3.SetActive(false);
+            feedbackwindow.SetActive(false);
+            Game5DragLogic.isTrueAnswer = false;
+            tryNum = 0;
+            checkBtn.gameObject.transform.gameObject.SetActive(true);
+        }
+        else if (numRound == 2)
+        {
+            numRound++;
+            round1.SetActive(false);
+            round2.SetActive(false);
+            round3.SetActive(true);
+            feedbackwindow.SetActive(false);
+            Game5DragLogic.isTrueAnswer = false;
+            tryNum = 0;
+            checkBtn.gameObject.transform.gameObject.SetActive(true);
+
+        }
+        else
+        {
+            influenceFeedBack.SetActive(true);
+            feedbackwindow.SetActive(false);
+        }
+
+    }
+}
+
+   
+
