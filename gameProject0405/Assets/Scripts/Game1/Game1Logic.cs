1f98ede4f28d756d04ba061bc2a706fd1414c582
mai yurman <mai.yurman777@gmail.com>
2022-05-26 17:44:38 +0200

    הגדרת סרטונים + כתוביות + בדיקת אודיו
    נשאר להקליט את האודיו כדי להתאים את הזמנים לכתוביות

--- gameProject0405/Assets/Scripts/Game1/Game1Logic.cs
+++ gameProject0405/Assets/Scripts/Game1/Game1Logic.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Game1Logic : MonoBehaviour
 {
@@ -9,16 +10,24 @@ public class Game1Logic : MonoBehaviour
     public Chat chatGame1;
     private int tryGame1;
     public GameObject finishGame1;
+    public GameObject video1;
+    public TextMeshProUGUI textVideo1;
 
     //משתנים משחק 2
     public Chat chatGame2;
     private int tryGame2;
     public GameObject finishGame2;
+    public GameObject video2;
+    public TextMeshProUGUI textVideo2;
+
 
     //משתנים משחק 3
     public Chat chatGame3;
     private int tryGame3;
     public GameObject finishGame3;
+    public GameObject video3;
+    public TextMeshProUGUI textVideo3;
+
 
     //סטורי
     private loadStoryBtn loadStoryBtn;
@@ -35,6 +44,8 @@ public class Game1Logic : MonoBehaviour
         chatGame1.setProfilePhoto("");
         chatGame1.addVideo(Chat.Direction.RECEIVE, "6", playVideo1);
         tryGame1 = 2;
+        video1.SetActive(false);
+        textVideo1.text = "";
 
         //משחק 2
         chatGame2.initChat();
@@ -42,6 +53,8 @@ public class Game1Logic : MonoBehaviour
         chatGame2.setProfilePhoto("");
         chatGame2.addVideo(Chat.Direction.RECEIVE, "6", playVideo2);
         tryGame2 = 2;
+        video2.SetActive(false);
+        textVideo2.text = "";
 
         //משחק 3
         chatGame3.initChat();
@@ -49,6 +62,8 @@ public class Game1Logic : MonoBehaviour
         chatGame3.setProfilePhoto("");
         chatGame3.addVideo(Chat.Direction.RECEIVE, "6", playVideo3);
         tryGame3 = 2;
+        video3.SetActive(false);
+        textVideo3.text = "";
 
         //כיבוי כל הסטורי
         loadStoryBtn.disableStoryBtnAll();
@@ -64,17 +79,101 @@ public class Game1Logic : MonoBehaviour
 
     private void playVideo1()
     {
-        Debug.Log("video 1");
+        video1.SetActive(true);
+        StartCoroutine(sentenceVideo1());
     }
 
     private void playVideo2()
     {
-        Debug.Log("video 2");
+        video2.SetActive(true);
+        StartCoroutine(sentenceVideo2());
+
     }
 
     private void playVideo3()
     {
-        Debug.Log("video 3");
+        video3.SetActive(true);
+        StartCoroutine(sentenceVideo3());
+
+    }
+
+
+    //-----> הפעלת כתוביות
+
+    IEnumerator sentenceVideo1()
+    {
+        yield return new WaitForSeconds(1);
+        textVideo1.text = "היי תמיר, שאלת בזמן האחרון למה נעלמתי והחלטתי פשוט לצלם לך סרטון ולתאר מה עבר עלי.";
+        yield return new WaitForSeconds(4);
+        textVideo1.text = "";
+        yield return new WaitForSeconds(1);
+        textVideo1.text = "הרגשתי בזמן האחרון שאין לי זמן לשום דבר.";
+        yield return new WaitForSeconds(4);
+        textVideo1.text = "";
+        yield return new WaitForSeconds(1);
+        textVideo1.text = "אני מנסה לשלב הכל חברים, עבודה, לימודים ותמיד אני מוצאת את עצמי מפספסת משהו. כולם תמיד נעלבים ממני.";
+        yield return new WaitForSeconds(4);
+        textVideo1.text = "";
+        yield return new WaitForSeconds(1);
+        textVideo1.text = "שום דבר לא יוצא כמו שאני רוצה.";
+        yield return new WaitForSeconds(4);
+        textVideo1.text = "";
+        yield return new WaitForSeconds(1);
+        textVideo1.text = "בשבוע שעבר, הבוס שלי ביקש ממני לעבוד כל השבוע והיה מבחן ביום רביעי אז מצאתי את עצמי לומדת בזמן המשמרת ואז לא התרכזתי לא בעבודה ולא בלימודים.";
+        yield return new WaitForSeconds(4);
+        textVideo1.text = "";
+        yield return new WaitForSeconds(1);
+        textVideo1.text = "אני לא יודעת מה לעשות.. מחכה לתגובה ממך.";
+        yield return new WaitForSeconds(4);
+        textVideo1.text = "";
+    }
+
+    IEnumerator sentenceVideo2()
+    {
+        yield return new WaitForSeconds(1);
+        textVideo2.text = "היי אחי, השעה 11:30 ירדתי בדיוק מהאוטו של יוסי המורה שלי לנהיגה.";
+        yield return new WaitForSeconds(4);
+        textVideo2.text = "";
+        yield return new WaitForSeconds(1);
+        textVideo2.text = "נהגתי כמעט שעה ואפילו עליתי על הכביש המהיר! אתה לא מבין איזה גז דפקתי! ";
+        yield return new WaitForSeconds(4);
+        textVideo2.text = "";
+        yield return new WaitForSeconds(1);
+        textVideo2.text = "המורה לנהיגה ממש התעצבן עלי הוא אמר שאני לא יכול לעשות מה שאני רוצה.";
+        yield return new WaitForSeconds(4);
+        textVideo2.text = "";
+        yield return new WaitForSeconds(1);
+        textVideo2.text = "אני חייב לחשוב לפני שאני מגיב.";
+        yield return new WaitForSeconds(4);
+        textVideo2.text = "";
+        yield return new WaitForSeconds(1);
+        textVideo2.text = "אני לא מבין מה הוא רוצה ממני! גם כשאני רב עם ההורים שלי אני כל קודם מדבר ורק אז חושב, אני מרגיש שאני לא מצליח לעצור ולרוב זה פוגע בי ובהם.";
+        yield return new WaitForSeconds(4);
+        textVideo2.text = "";
+        yield return new WaitForSeconds(1);
+        textVideo2.text = "אני לא מבין מה הבעיה שלי.";
+        yield return new WaitForSeconds(4);
+        textVideo2.text = "";
+    }
+
+    IEnumerator sentenceVideo3()
+    {
+        yield return new WaitForSeconds(1);
+        textVideo3.text = "היי תמיר, אתה לא מאמין מה קרה לי?";
+        yield return new WaitForSeconds(4);
+        textVideo3.text = "";
+        yield return new WaitForSeconds(1);
+        textVideo3.text = "אתה זוכר שסיפרתי לך שאני הולכת למיונים ליחידה של 8200 של חיל המודיעין ואני בדוק עוברת?";
+        yield return new WaitForSeconds(4);
+        textVideo3.text = "";
+        yield return new WaitForSeconds(1);
+        textVideo3.text = "מרוב שהייתי בטוחה בעצמי אפילו לא התכוננתי למבחנים וכבר סיפרתי לכולם שבטוח התקבלתי.";
+        yield return new WaitForSeconds(4);
+        textVideo3.text = "";
+        yield return new WaitForSeconds(1);
+        textVideo3.text = "שובצתי בכלל להיות תצפיתנית, אני לא מבינה איך זה קרה.";
+        yield return new WaitForSeconds(4);
+        textVideo3.text = "";
     }
 
 
